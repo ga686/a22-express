24edd914c8ea241634bb43ff84251f60cdb15303
Penny Chang <yahusanchang@gmail.com>
2022-08-05 15:50:10 +0800

    新增註解

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
-
+// 設定express-handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+// 設定路由
 app.get('/', (req, res) => {
   res.render('index')
 })
@@ -21,7 +21,7 @@ app.get('/portfolio', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact')
 })
-
+// 設置靜態檔案路徑
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Express is running on http://localhost:${port}`))
